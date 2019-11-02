f4a07443554541144fd413e5dabf6a450f882be8
David Riveraperez <david.i.riveraperez@gmail.com>
2019-11-01 23:22:58 -0500

    first commit not sure why isn't going thur all the questions

---
+++ questions.js
@@ -0,0 +1,33 @@
+// Questions 
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "object", "alerts", " undefined","numbers", "boolean"],
+        answer: "alerts"
+    },
+    
+    {
+        title: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    
+    {
+        title: "Which of the following is not a reserved word in JavaScript?",
+        choices: ["interface","throws", "program", "short"],
+        answer: "program"
+    },
+
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    
+];
---
+++ script.js
@@ -0,0 +1,151 @@
+var secondsLeft = 75;
+var timerInterval;
+
+var timeEl = document.querySelector(".Time");
+var questionsEl = document.querySelector(".questions-rendered");
+var scoreEl = document.querySelector(".Score");
+var highScoreEl = document.querySelector(".highScore");
+var submitButton = document.querySelector("#submit");
+var refreshButton = document.querySelector("#goBack");
+
+var initialInput = document.querySelector("#initials");
+var scoreList = document.querySelector("#scoreList");
+var scoreForm = document.querySelector("#score-form");
+
+var userInitials = [];
+
+document.querySelector('.Score').style.display='none';
+document.querySelector('.highscorePage').style.display='none';
+
+refreshButton.addEventListener("click", function(){
+    location.reload();
+});
+
+function setTime(){
+    timerInterval = setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = "TIME REMAINING: " + secondsLeft;
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+            document.querySelector(".Score").style.display="block";
+            document.querySelector(".Time").style.display="none";
+            document.querySelector(".questions-rendered").style.display="none";
+            highScoreEl.textContent = "YOUR SCORE IS " + secondsLeft;
+        }
+    }, 1000);
+}
+
+const startBtn = document.getElementById("startquiz");
+
+startBtn.addEventListener('click', function() {
+    setTime();
+    startBtn.style.display = 'none';
+    document.querySelector('.splashPage').style.display='none';
+    displayQuestions(questionIndex);
+});
+
+var questionIndex = 0;
+
+function displayQuestions(){
+    
+    questionsEl.textContent = "";
+    var question = questions[questionIndex];
+    var questionDiv = document.createElement("div");
+    var questionText = document.createElement("h2");
+    questionText.textContent = question.title;
+    questionDiv.appendChild(questionText);
+    
+    for (var i=0; i < question.choices.length ;i++) {
+        var option = document.createElement("button");
+        option.textContent = question.choices[i];
+        option.setAttribute("class", "option");
+        option.addEventListener("click", function(e) {
+            var optionClicked = (e.target.innerHTML);
+            
+            if(optionClicked === questions[questionIndex].answer){
+                alert("Correct!");
+                displayQuestions(questionIndex++);
+            }
+            else{
+                alert("Incorrect!");
+                displayQuestions(questionIndex++);
+                secondsLeft -= 15;
+            }
+            
+        });
+        if (questionIndex == questions.length - 1) {
+            clearInterval(timerInterval);
+            document.querySelector('.Score').style.display='block';
+            document.querySelector(".Time").style.display='none';
+            highScoreEl.textContent = "YOUR SCORE IS " + secondsLeft;
+            return;
+        }
+        questionDiv.appendChild(option);
+    }
+    questionsEl.appendChild(questionDiv);
+}
+
+function renderScores(){
+    scoreList.innerHTML = "";
+    
+    for (var x = 0; x < userInitials.length; x ++){
+        var letters = userInitials[x];
+        
+        var li = document.createElement("li");
+        li.textContent = letters;
+        li.setAttribute("data-index", x);
+        
+        var button = document.createElement("button");
+        button.textContent = "Clear";
+        
+        li.appendChild(button);
+        scoreList.appendChild(li);
+    }
+} 
+
+function init() {
+    var storedScores = JSON.parse(localStorage.getItem("userInitials"));
+    
+    if(storedScores !==null) {
+        userInitials = storedScores;
+    }
+    
+    const submitBtn = document.getElementById("submit");
+    
+    submitBtn.addEventListener('click', function() {
+        event.preventDefault();
+        
+        var initialText = initialInput.value.trim();
+        
+        if (initialText === ""){
+            return;
+        }
+        userInitials.push(initialText + " " + secondsLeft);
+        console.log(userInitials);
+        initialInput.value = "";
+        
+        document.querySelector('.Score').style.display='none';
+        document.querySelector('.highscorePage').style.display='block';
+        
+        storeScore();
+        renderScores();
+        
+    });
+}
+
+function storeScore(){
+    localStorage.setItem("userInitials", JSON.stringify(userInitials));
+}
+
+scoreList.addEventListener("click", function(event){
+    var element = event.target;
+    
+    if (element.matches("button")=== true){
+        var index = element.parentElement.getAttribute("data-index");
+        userInitials.splice(index, 1);
+        
+        storeScore();
+        renderScores();
+    }
+});
+init();
